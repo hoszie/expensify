90da2099ade7ecdbc0cdf1e3f57893af7dc8f766
Nick <nhoszko@gmail.com>
2020-02-11 01:10:41 +0000

    incorporated css into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modles/
-    }]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }
+  ]
   },
   devtool: "cheap-module-eval-source-map",
   devServer: {
