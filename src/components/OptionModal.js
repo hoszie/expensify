7a3c0014e5fb6ab5e3cf6b59d5591d7947038b53
Nick Hoszko <nickhoszko@Nicks-MBP.vc.shawcable.net>
2020-02-15 18:04:22 -0800

    Completed css for desktop

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
     onRequestClose={props.handleSelectedOption}
     ariaHideApp={false} 
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleSelectedOption}>Okay!</button>
+    <h3 className='modal__title'>Selected Option</h3>
+    {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+    <button className='button' onClick={props.handleSelectedOption}>Okay!</button>
   </Modal>
 );
 export default OptionModal;
