70088ed6098112ee0baed39a49b79f172de90a43
Nick Hoszko <nickhoszko@Nicks-MacBook-Pro.local>
2020-02-18 13:07:37 -0800

    update

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFoundPage = () => (
   <div>
     {/* 404! - <a href={"/"}>Go home</a> */}
-    404! - <Link to='/'>Go Home</Link>
+    404! - <Link to='/'>Go Home</Link> 
   </div>
 );
 
