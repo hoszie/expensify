624ca6c0c1bff913577d8fb6b624a375f011ba95
Nick Hoszko <nickhoszko@Nicks-MBP.vc.shawcable.net>
2020-02-26 21:37:56 -0800

    end of Redux chapter

--- src/playground/Redux101.js
+++ src/playground/Redux101.js
@@ -1,21 +1,34 @@
 import { createStore } from 'redux';
 
-const incrementCount = () => ({
-  type: 'INCREMENT'
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
 });
 
-const store = createStore((state = { count: 0 }, action) => {
-  console.log(action.incrementBy);
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+})
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+/// REDUCERS ////
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
       case 'SET':
         return {
@@ -28,38 +41,28 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// store.dispatch({
-//   type: 'INCREMENT',
-//   incrementBy: 5
-// });
+
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
 store.dispatch(incrementCount());
 
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'INCREMENT'
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 8
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 160
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({ count: 111 }));
 
 
 
