7003b81576245d5c832176884b64b9dfbe3eda38
Nick Hoszko <nhoszko@gmail.com>
2020-03-07 20:46:25 -0800

    set up production  build. env production flag

--- src/app.js
+++ src/app.js
@@ -12,15 +12,15 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-store.dispatch(addExpense({ description: 'Drugs', amount: 19500 }));
-store.dispatch(addExpense({ description: 'Groceries', amount: 1500 }));
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+// store.dispatch(addExpense({ description: 'Drugs', amount: 19500 }));
+// store.dispatch(addExpense({ description: 'Groceries', amount: 1500 }));
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -5,7 +5,7 @@ import { editExpense, removeExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
   onSubmit= (expense) => {
-    this.props.editExpense(props.expense.id, expense);
+    this.props.editExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   };
   onRemove = () => {
